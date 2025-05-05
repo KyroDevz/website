c08aff6103b4bf2674faafdd3afa9d9680cf5bb7
KyroDevz <kyrodevz@hotmail.com>
2025-05-05 18:33:44 +0100

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -27,4 +27,18 @@ export const projects = [
         description: "Allthough they still use my assets without permission during my time at this establishment I helped create a lot of gameplay mechanics from the panic button system, fires system, re-worked gun system and more! (Egmo if you're reading this check your Emails from the Copyright Agent!)",
         link: "https://www.roblox.com/games/17428786424/AIRPORT-City-of-London"
       },
+          {
+        id: 6,
+        title: "999 Operator Simulator | Solo Project",
+        image: "https://tr.rbxcdn.com/180DAY-048c0c8207084d71c696bc1532343ace/768/432/Image/Png/noFilter",
+        description: "When making this game I was the solo developer, creating the UI over a couple of months ago then scripting a whole game around it. This is an early prototype and the new release is expected soon.",
+        link: "https://www.roblox.com/games/12921861191/999-Operator-Simulator-Roblox"
+      },
+          {
+        id: 7,
+        title: "Project Island | Solo Project",
+        image: "https://tr.rbxcdn.com/180DAY-17cf6286a919635ecdadd1f894f861e3/768/432/Image/Webp/noFilter",
+        description: "This solo project is unfinished and is still in the works, however I am responsable for all of the programming in this project!",
+        link: "https://www.roblox.com/games/13601391174/Project-Island"
+      },
   ]
